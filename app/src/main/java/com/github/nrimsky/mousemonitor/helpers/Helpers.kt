ada3e6456312aeaed0422ac00a84de4399ffbd3f
Nina <ninarimsky@gmail.com>
2021-03-05 23:31:14 +0000

    update for raw frequency input

--- app/src/main/java/com/github/nrimsky/mousemonitor/helpers/Helpers.kt
+++
@@ -1,9 +0,0 @@
-package com.github.nrimsky.mousemonitor
-
-// TODO: Allow user to pick the delimiter
-fun floatListFromString(str: String): List<Float> {
-    return str
-        .split(",")
-        .filter { it != "" }
-        .map { it.toFloat() }
-}
--- app/src/main/java/com/github/nrimsky/mousemonitor/ui/MainActivity.kt
+++ app/src/main/java/com/github/nrimsky/mousemonitor/ui/MainActivity.kt
@@ -21,16 +21,12 @@ import com.github.nrimsky.mousemonitor.service.BluetoothService
 import com.github.nrimsky.mousemonitor.helpers.Constants.MESSAGE_CONNECTED
 import com.github.nrimsky.mousemonitor.helpers.Constants.MESSAGE_DISCONNECTED
 import com.github.nrimsky.mousemonitor.helpers.Constants.MESSAGE_READ
-import com.github.nrimsky.mousemonitor.dataprocessing.FFTManager
-import com.github.nrimsky.mousemonitor.dataprocessing.MAFilterManager
 import com.github.nrimsky.mousemonitor.databinding.ActivityMainBinding
 import com.github.nrimsky.mousemonitor.dataprocessing.ThresholdManager
-import com.github.nrimsky.mousemonitor.floatListFromString
 import com.github.nrimsky.mousemonitor.helpers.setupNumberTextField
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import kotlin.math.pow
 
 
 class MainActivity : AppCompatActivity() {
@@ -40,36 +36,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var data: LineData
-    private var dataSet: LineDataSet = LineDataSet(mutableListOf<Entry>(), "Piezo readings")
-
-    private lateinit var fftData: LineData
-    private var fftDataSet: LineDataSet = LineDataSet(
-        mutableListOf<Entry>(),
-        "FFT (Beats per Minute)"
-    )
-    private var t = 0
+    private var dataSet: LineDataSet = LineDataSet(mutableListOf<Entry>(), "Breathing Rate")
 
     // Initialise with default start values
-    private var fftManager = FFTManager(DEFAULT_FFT_LEN)
-    private var fftSize: Int = DEFAULT_FFT_LEN
-    private var maManager = MAFilterManager(DEFAULT_MA_SIZE)
     private var thresholdManager = ThresholdManager(DEFAULT_LOWER_THRESHOLD, DEFAULT_UPPER_THRESHOLD)
-    private var samplingFrequency = DEFAULT_SAMPLING_FREQUENCY
+
+    private val maBuf = Array(5){0F}
+    private var maTot = 0F
+    private var maI = 0
+    private var t = 0
 
     companion object {
         private const val PERMISSION_CODE = 1
         private const val REQUEST_ENABLE_BT = 2
         private const val TAG = "MainActivity"
-        // In Hz
-        private const val DEFAULT_SAMPLING_FREQUENCY = 10
-        private const val DEFAULT_MA_SIZE = 1
-        private const val MA_MAX_SIZE = 10
-        private const val MA_MIN_SIZE = 1
-        private const val DEFAULT_FFT_LEN = 1024
         private const val DEFAULT_LOWER_THRESHOLD = 55f
         private const val DEFAULT_UPPER_THRESHOLD = 65f
-        // 128, 256, 512, 1024, 2056
-        private val FFT_LEN_OPTIONS = arrayOf(7, 8, 9, 10, 11).map { 2f.pow(it).toString() }.toTypedArray()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -79,12 +61,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         setupGraph()
-        setupFFTGraph()
-        setupSamplingFrequencyInput()
-        setupMovingAverage()
-        setupFFTSizePicker()
         setupThresholdChoosers()
-
         checkBluetoothPermissions()
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
@@ -130,6 +107,8 @@ class MainActivity : AppCompatActivity() {
         bluetoothService?.cancel()
     }
 
+    private var chars: String = ""
+
     // Message handler for data received from bluetooth service
     private val mHandler = object : Handler(Looper.getMainLooper()) {
         @SuppressLint("SetTextI18n")
@@ -139,7 +118,14 @@ class MainActivity : AppCompatActivity() {
                     val readBuf = msg.obj as ByteArray
                     // construct a string from the valid bytes in the buffer
                     val readMessage = String(readBuf, 0, msg.arg1)
-                    processData(readMessage)
+                    for (letter in readMessage) {
+                        chars = if (letter.toString() == ",") {
+                            processData(chars.toInt().toFloat())
+                            ""
+                        } else {
+                            chars.plus(letter)
+                        }
+                    }
                 }
                 MESSAGE_CONNECTED -> {
                     val deviceName = msg.obj as String
@@ -154,14 +140,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SetTextI18n")
-    private fun processData(readMessage: String) {
-        val readings = floatListFromString(readMessage)
-        showFFTOnScreen(fftManager.nextValues(readings))
-        addPointsToGraph(maManager.nextValues(readings))
-        val breathingRate = fftManager.maxFreq(samplingFrequency)
-        val mouseState = thresholdManager.getState(breathingRate)
-        binding.mainBpmDisplay.text = "$breathingRate BPM"
-        binding.mainBpmDisplay.setBackgroundColor(getColor(mouseState))
+    private fun processData(periodMilliseconds: Float) {
+        val periodSeconds = periodMilliseconds / 1000F
+        val freqHz = 1F/periodSeconds
+        val freqBPM = 60F*freqHz
+        // Extra check for "sensible" value in case there was a bluetooth transmission lost bit
+        if ((freqBPM > 30) && (freqBPM < 100)) {
+            if  (maI >= maBuf.size) {
+                maI = 0
+            }
+            maTot-=maBuf[maI]
+            maBuf[maI] = freqBPM
+            maTot+=maBuf[maI]
+            maI++
+
+            val newReading = maTot/maBuf.size
+            addPointToGraph(newReading)
+            val mouseState = thresholdManager.getState(newReading)
+            binding.mainBpmDisplay.text = "$newReading BPM"
+            binding.mainBpmDisplay.setBackgroundColor(getColor(mouseState))
+        }
     }
 
     // Bluetooth
@@ -214,46 +212,7 @@ class MainActivity : AppCompatActivity() {
         binding.linearLayout.addView(button)
     }
 
-    // FFT
-
-    private fun showFFTOnScreen(fft: FloatArray) {
-        val frequencyResolution = samplingFrequency.toFloat()/fftSize.toFloat()
-        fftDataSet.clear()
-        fft.forEachIndexed { i, fftVal ->
-            fftDataSet.addEntry(Entry(i * frequencyResolution * 60, fftVal))
-        }
-        fftData.notifyDataChanged()
-        binding.fftChart.notifyDataSetChanged()
-        binding.fftChart.invalidate()
-    }
-
-    private fun setupFFTGraph() {
-        with(binding.fftChart) {
-            setBackgroundColor(Color.WHITE)
-            axisLeft.isEnabled = true
-            axisRight.isEnabled = false
-            xAxis.isEnabled = true
-            xAxis.position = XAxis.XAxisPosition.BOTTOM
-            xAxis.setDrawLabels(true)
-            legend.isEnabled = false
-            description.isEnabled = false
-            axisLeft.setDrawGridLines(false)
-            xAxis.setDrawGridLines(true)
-            axisLeft.setDrawZeroLine(true)
-            xAxis.axisMaximum = 100f
-            xAxis.axisMinimum = 10f
-        }
-        with(fftDataSet) {
-            color = Color.BLUE
-            lineWidth = 2f
-            setDrawCircles(false)
-            setDrawValues(false)
-        }
-        fftData = LineData(fftDataSet)
-        binding.fftChart.data = fftData
-    }
-
-    // Main Graph
+    // Graph
 
     private fun setupGraph() {
         with(binding.chartView) {
@@ -267,7 +226,7 @@ class MainActivity : AppCompatActivity() {
             description.isEnabled = false
             axisLeft.setDrawGridLines(false)
             xAxis.setDrawGridLines(false)
-            axisLeft.axisMaximum = 250f
+            axisLeft.axisMaximum = 100f
             axisLeft.axisMinimum = 0f
             axisLeft.setDrawZeroLine(true)
         }
@@ -282,22 +241,6 @@ class MainActivity : AppCompatActivity() {
         binding.chartView.data = data
     }
 
-    private fun addPointsToGraph(points: List<Float>) {
-        val dataPoints = points.map { dataPoint ->
-            t += 1
-            Entry(t.toFloat(), dataPoint)
-        }
-        dataPoints.forEach {
-            dataSet.addEntry(it)
-            if (dataSet.values.size > 100) {
-                dataSet.removeFirst()
-            }
-        }
-        data.notifyDataChanged()
-        binding.chartView.notifyDataSetChanged()
-        binding.chartView.invalidate()
-    }
-
     private fun addPointToGraph(point: Float) {
         t += 1
         val dataPoint = Entry(t.toFloat(), point)
@@ -310,56 +253,6 @@ class MainActivity : AppCompatActivity() {
         binding.chartView.invalidate()
     }
 
-    // Moving average picker
-
-    private fun setupMovingAverage() {
-        with(binding.movingAverageSizePicker) {
-            maxValue = MA_MAX_SIZE
-            minValue = MA_MIN_SIZE
-            value = DEFAULT_MA_SIZE
-        }
-        binding.movingAverageSizePicker.setOnValueChangedListener { _, _, newVal ->
-            Log.d(TAG, "Moving average filter size changed to $newVal")
-            maManager = MAFilterManager(newVal)
-        }
-    }
-
-    // FFT Size picker
-
-    private fun setupFFTSizePicker() {
-        with(binding.fftSizePicker) {
-            maxValue = FFT_LEN_OPTIONS.size - 1
-            minValue = 0
-            displayedValues = FFT_LEN_OPTIONS
-            // Corresponds to 1024 samples
-            value = 3
-        }
-        binding.fftSizePicker.setOnValueChangedListener { _, _, newVal ->
-            fftSize = 2f.pow(newVal + 7).toInt()
-            Log.d(TAG, "FFT size changed to $fftSize")
-            fftManager = FFTManager(fftSize)
-        }
-    }
-
-    // Sampling frequency input
-
-    private fun setupSamplingFrequencyInput() {
-        setupNumberTextField(binding.samplingFreqInput, DEFAULT_SAMPLING_FREQUENCY.toString())
-        binding.samplingFreqInput.setOnEditorActionListener { _, _, _ ->
-            var handled = false
-            binding.samplingFreqInput.text.toString().toIntOrNull()?.let {
-                samplingFrequency = it
-                Log.d(TAG, "Sampling frequency changed to $samplingFrequency")
-                hideKeyboard()
-                handled = true
-            }?:run{
-                binding.samplingFreqInput.setText(DEFAULT_SAMPLING_FREQUENCY.toString())
-                hideKeyboard()
-            }
-            return@setOnEditorActionListener handled
-        }
-    }
-
     fun hideKeyboard() {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
         //Find the currently focused view, so we can grab the correct window token from it.
@@ -367,8 +260,6 @@ class MainActivity : AppCompatActivity() {
             imm?.hideSoftInputFromWindow(it.windowToken, 0)
         }
     }
-
-
 
     // Threshold choosers
 
@@ -421,8 +312,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-
-
 
 }
